4e425f4bd12da20b5d51c5ced2b972366c80061c
ShraiL <lashraieri@gmail.com>
2025-06-03 21:23:42 +0400

    Create {EntityNamePlural}Database.sql

---
+++ {EntityNamePlural}Database.sql
@@ -0,0 +1,24 @@
+-- {EntityNamePlural}Database.sql
+-- IMPORTANT: Replace {EntityNamePlural} with the plural name of your entity (e.g., Cars, Cats, Persons)
+-- IMPORTANT: Replace {Property1Name}, {Property2Name}, {Property3Name} with your actual column names.
+-- IMPORTANT: Replace {Property1Type}, {Property2Type}, {Property3Type} with your actual SQL data types (e.g., NVARCHAR(255), FLOAT, INT).
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'{EntityNamePlural}Database')
+BEGIN
+    CREATE DATABASE {EntityNamePlural}Database;
+END
+GO
+
+USE {EntityNamePlural}Database;
+GO
+
+IF NOT EXISTS (SELECT * FROM SYSOBJECTS WHERE NAME='{EntityNamePlural}' AND XTYPE='U')
+BEGIN
+    CREATE TABLE {EntityNamePlural} (
+        Id INT PRIMARY KEY IDENTITY(1,1),
+        {Property1Name} {Property1Type} NOT NULL, -- e.g., Model NVARCHAR(255)
+        {Property2Name} {Property2Type} NOT NULL, -- e.g., Weight FLOAT
+        {Property3Name} {Property3Type} NOT NULL  -- e.g., Speed INT
+    );
+END
+GO
